1ebaffd6aa6a1ec046fe04f27fc40cd41d29d951
Khaled Oghli <khalidoghli7@gmail.com>
2024-07-12 18:36:34 +0400

    updated

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,6 +1,10 @@
 import type { RouterConfig } from '@nuxt/schema';
 
 export default <RouterConfig>{
+  stringifyQuery: (query) => {
+    const keys = Object.keys(query);
+    return keys.map(key => `${key}=${encodeURIComponent(query[key] as string)}`).join('&');
+  },
   async scrollBehavior(to, from, savedPosition) {
     const nuxtApp = useNuxtApp();
 
